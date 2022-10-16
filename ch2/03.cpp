1806057b30a5081cf7c63813d5c22810ae753736
BWsix <radioactive-lollipop@proton.me>
2022-10-16 18:58:55 +0800

    solution: 2-3

---
+++ ch2/03.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> vi;
+#define pb push_back
+
+LL x, y, z;
+
+void reader() { cin >> x >> y >> z; }
+
+LL solver(int power) {
+  if (power == 0) return 1;
+  if (power % 2 == 1) return (solver(power - 1) * x) % z;
+
+  LL result = solver(power / 2);
+  return (result * result) % z;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0);
+
+  reader();
+  cout << solver(y);
+
+  return 0;
+}
